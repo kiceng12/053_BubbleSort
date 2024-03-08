163cad758f0ace2575ee38275ad3b3ccd185d6c5
kiceng12 <sukmahendrawan12@gmail.com>
2024-03-08 16:37:56 +0700

    return

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -54,5 +54,43 @@ void bubbleSortArray() { // procedur untuk mengurutkan array dengan metode bubbl
 		cout << endl;
 	} while (pass <= n - 1);  //step 5
 
-
 }
+
+
+		
+void display() {
+	cout << endl;
+	cout << "================================" << endl;
+	cout << "Elemen Array yang telah tersusun" << endl;
+	cout << "================================" << endl;
+	cout << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j]; //menampikan array
+		if (j < n - 1) {
+			cout << "-->";
+		}
+	}
+	cout << endl;
+	cout << endl;
+
+	cout << "Jumlah pass = " << n - 1 << endl; // menampilkan jumlah dari pass
+	cout << endl;
+	cout << endl;
+	
+}
+
+int main() {
+
+	input();
+
+	bubbleSortArray();
+	display();
+
+	system("pause");
+	return 0;
+}
+			
+	
+
+
+
