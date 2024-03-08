3cbf859be29c32e879f04130b52093fb8bc049ab
kiceng12 <sukmahendrawan12@gmail.com>
2024-03-08 15:27:42 +0700

    deklarasi variabel

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -8,6 +8,3 @@ using namespace std;
 int arr[20]; // Deklarasi variabel global array a dengan uuran 20
 int n; // Deklarasi variabel global n untuk menyimpan banyaknya elemen array
 
-int main(
-
-)
